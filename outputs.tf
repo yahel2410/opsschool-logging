77a5f9130361c9fbe22b31c111c46fdb1771b883
yahel <yahel2410@gmail.com>
2022-02-28 20:48:53 +0200

    Add private ip to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,14 @@
 output "public_ip" {
   value = module.ec2-instance.public_ip[0]
 }
+output "private_ip" {
+  value = module.ec2-instance.private_ip[0]
+}
+
+
+output "ssh_cmd" {
+  value = "ssh ubuntu@${module.ec2-instance.public_ip[0]}"
+}
 output "kibana_url" {
   value = "http://${module.ec2-instance.public_ip[0]}:5601"
 }
