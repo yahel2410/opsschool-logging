08e3cb3e3b1e6cfcda3d644d7027b5c043848b1d
yahel <yahel2410@gmail.com>
2022-03-01 13:09:09 +0200

    Update userdata.sh: - Install mysql - Open ES on all networs - Collect container logs

--- userdata.sh
+++ userdata.sh
@@ -2,10 +2,13 @@
 set -e
 
 echo "INFO: userdata started"
+export DEBIAN_FRONTEND=noninteractive
 
 # elasticsearch
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.10.2-amd64.deb
 dpkg -i elasticsearch-*.deb
+echo 'network.host: 0.0.0.0' >> /etc/elasticsearch/elasticsearch.yml
+echo 'discovery.type: single-node' >> /etc/elasticsearch/elasticsearch.yml
 systemctl enable elasticsearch
 systemctl start elasticsearch
 
@@ -20,6 +23,16 @@ systemctl start kibana
 wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.11.0-amd64.deb
 dpkg -i filebeat-*.deb
 
+# mysql
+apt update
+apt install mysql-server -y
+systemctl enable mysql
+systemctl start mysql
+
+# docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker ubuntu
 
 sudo mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.BCK
 
@@ -30,6 +43,11 @@ filebeat.inputs:
     paths:
       - /var/log/auth.log
 
+  - type: container
+    enabled: false
+    paths:
+      - "/var/lib/docker/containers/*/*.log"
+
 filebeat.modules:
   - module: system
     syslog:
@@ -37,11 +55,17 @@ filebeat.modules:
     auth:
       enabled: false
 
+  - module: mysql
+    error:
+      enabled: false
+    slowlog:
+      enabled: false
+
 filebeat.config.modules:
   path: ${path.config}/modules.d/*.yml
   reload.enabled: false
 
-setup.dashboards.enabled: false
+setup.dashboards.enabled: true
 
 setup.template.name: "filebeat"
 setup.template.pattern: "filebeat-*"
@@ -52,6 +76,8 @@ processors:
   - add_host_metadata:
       when.not.contains.tags: forwarded
   - add_cloud_metadata: ~
+  - add_docker_metadata:
+      host: "unix:///var/run/docker.sock"
 
 output.elasticsearch:
   hosts: [ "localhost:9200" ]
